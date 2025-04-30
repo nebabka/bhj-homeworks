d4d8098c8198b3315682c4e653ccd8903b336f6c
nebabka <nastyfedinadura@gmail.com>
2025-04-30 15:17:04 +0300

    Задание печенька-кликер1

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -2,7 +2,12 @@ const cookie = document.getElementById("cookie");
 const clicker = document.getElementById("clicker__counter");
 let clickerNum = 0;
 
-cookie.addEventListener('click', () => {
+cookie.addEventListener('mousedown', function() {
     clickerNum = clickerNum + 1;
     clicker.textContent = clickerNum;
+    this.width *= 1.1;
+});
+
+cookie.addEventListener('mouseup', function() {
+    this.width /= 1.1;
 });
