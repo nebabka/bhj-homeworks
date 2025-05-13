e6bb8ad9afc047d0e231bfb5cfa36829a642be9e
nebabka <nastyfedinadura@gmail.com>
2025-05-13 19:29:42 +0300

    Задание сделаны1

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -6,12 +6,16 @@ function remove() {
         book.classList.remove('font-size_small');
         book.classList.remove('font-size_big');
     });
+    links.forEach(link => {
+        link.classList.remove('font-size_active');
+    });
 };
 
 links.forEach(link => {
     link.addEventListener('click', (event) => {
         event.preventDefault();
         remove();
+        link.classList.add('font-size_active');
 
         if(link.classList.length > 1){
         books.forEach(book => book.classList.add(link.classList[1]));
